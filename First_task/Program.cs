449f7402a7a252f0129f7b6c99dee3d136e9b5ad
Elena <gudulina@mail.ru>
2022-06-30 22:29:16 +0700

    First task

---
+++ First_task/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+void Palindrome(int n)
+{
+    int i = 0;
+    string num = n.ToString();
+    int size = num.Length - 1;
+    string flag = "Palindrome";
+
+    while (i < size / 2)
+    {
+        if (num[i] != num[size - i])
+        {
+            flag = "Not a palindrome";
+            break;
+        }
+        i++;
+    }
+    Console.WriteLine($"{n} - {flag}");
+}
+
+Palindrome(14212);
+Palindrome(12821);
+Palindrome(23432);
